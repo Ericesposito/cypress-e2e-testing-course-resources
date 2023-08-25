c71045bca105572848a6e4fa4f36e18f01015a9b
Ericesposito <eespo93@optonline.net>
2023-08-25 08:56:36 -0400

    managed to work on get, contains, should, find, click methods

--- course-work/02 Starting Project/cypress/e2e/basics.cy.js
+++ course-work/02 Starting Project/cypress/e2e/basics.cy.js
@@ -3,6 +3,13 @@
 describe('tasks page', () => {
   it('should render the main image', () => {
     cy.visit('http://localhost:5173/');
-    // cy.get()
+    cy.get('.main-header').find('img');
+  })
+
+  it('should display the page title', () => {
+    cy.visit('http://localhost:5173/');
+    cy.get('h1').should('have.length', 1);
+    cy.get('h1').contains('My Cypress Course Tasks');
+    // cy.contains('My Cypress Course Tasks')
   })
 })
